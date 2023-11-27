617713f43e81da526647c1523d60c1057e5acbde
Shubham Verma <vermashubham2707@gmail.com>
2023-11-27 18:39:09 +0530

    -- installed bootstrap, axios, router, sass

--- crud-app/src/App.js
+++ crud-app/src/App.js
@@ -1,23 +1,23 @@
-import logo from "./logo.svg";
 import "./App.css";
-
+import Create from "./Components/Create";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Read from "./Components/Read";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1 style={{ textAlign: "center", marginTop: "50px" }}>
+        This is React Crud Application
+      </h1>
+      <BrowserRouter>
+        <div className="routes">
+          <Routes>
+            <Route exact path="/" element={<Create />}></Route>
+            <Route exact path="/read" element={<Read />}></Route>
+            <Route exact path="/" element={<Create />}></Route>
+            <Route exact path="/" element={<Create />}></Route>
+          </Routes>
+        </div>
+      </BrowserRouter>
     </div>
   );
 }
