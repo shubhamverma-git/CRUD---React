83e1392ebcca90ea3c387fcb11a0b1fb091dd700
Shubham Verma <vermashubham2707@gmail.com>
2023-12-03 17:59:56 +0530

    -- create and read data successfully

--- crud-app/src/Components/Create.js
+++ crud-app/src/Components/Create.js
@@ -1,104 +1,82 @@
-// import React, { useState } from "react";
-// import classes from "./Create.module.scss";
-// import axios from "axios";
-// import { useNavigate } from "react-router-dom";
-
-// function Create() {
-//   const [name, setName] = useState("");
-//   const [email, setEmail] = useState("");
-//   const [phone, setPhone] = useState("");
-//   const history = useNavigate();
-
-//   const handleSubmit = (e) => {
-//     e.preventDefault();
-//     axios
-//       .post(
-//         "https://crudcrud.com/api/f152f3202eea4d98a7403808886a92ca/formData",
-//         {
-//           name: name,
-//           email: email,
-//           phone: phone,
-//         }
-//       )
-
-//       .then(() => {
-//         history("/read");
-//       });
-//   };
-
-//   return (
-//     <div className={classes.container}>
-//       <h1 className="bg-warning p-2">Register Here</h1>
-//       <div
-//         className={`d-flex justify-content-center mt-5 ${classes.form_container}`}
-//       >
-//         <form className={classes.form}>
-//           <div className="mb-3">
-//             <label htmlFor="userName" className="form-label">
-//               <h1>Full Name</h1>
-//             </label>
-//             <input
-//               type="text"
-//               onChange={(e) => setName(e.target.value)}
-//               className={`form-control ${classes.input}`}
-//               id="userName"
-//             />
-//           </div>
-//           <div className="mb-3">
-//             <label htmlFor="userEmail" className="form-label">
-//               <h1>Email</h1>
-//             </label>
-//             <input
-//               type="email"
-//               onChange={(e) => setEmail(e.target.value)}
-//               className={`form-control ${classes.input}`}
-//               id="userEmail"
-//             />
-//           </div>
-//           <div className="mb-3">
-//             <label htmlFor="userPhone" className="form-label">
-//               <h1>Contact Number</h1>
-//             </label>
-//             <input
-//               type="tel"
-//               onChange={(e) => setPhone(e.target.value)}
-//               className={`form-control ${classes.input}`}
-//               id="userPhone"
-//             />
-//           </div>
-//           <button
-//             type="submit"
-//             onClick={handleSubmit}
-//             className="btn btn-primary mt-5 "
-//           >
-//             Register
-//           </button>
-//         </form>
-//       </div>
-//     </div>
-//   );
-// }
-
-// export default Create;
-
 import React, { useState } from "react";
-import ImageCorousel from "./ImageCorousel";
+import classes from "./Create.module.scss";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
 
 function Create() {
-  const [name, setName] = useState("naman");
-  const handleClick = (e) => {
-    setName("aman");
-  };
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [phone, setPhone] = useState("");
+  const history = useNavigate();
 
-  //   const arr = useState("fakljl");
-  //   const name = arr[0];
-  //   const setName = arr[1];
+  const handleSubmit = (e) => {
+    // console.log("Warning");
+    e.preventDefault();
+    axios.post(
+      "https://crudcrud.com/api/5cf2ba86a1934575b3a661e18dc71377/formData",
+      {
+        name: name,
+        email: email,
+        phone: phone,
+      }
+    );
+
+    history("/read");
+  };
   return (
-    <div>
-      <h1>{name}</h1>
-      <button onClick={() => setName("Hello")}>Click me</button>
-      <div className="mt-5">
-        <ImageCorousel />
+    <div className={classes.container}>
+      <h1 className="bg-warning p-2">Register Here</h1>
+      <div
+        className={`d-flex justify-content-center mt-5 ${classes.form_container}`}
+      >
+        <form onSubmit={handleSubmit}>
+          <div className="mb-3">
+            <label for="userName" className="form-label">
+              <h1>Full Name</h1>
+            </label>
+            <input
+              type="text"
+              onChange={(e) => setName(e.target.value)}
+              className="form-control"
+              id="userName"
+            />
+          </div>
+          <div className="mb-3">
+            <label for="userEmail" className="form-label">
+              <h1>Email</h1>
+            </label>
+            <input
+              type="email"
+              onChange={(e) => setEmail(e.target.value)}
+              className="form-control"
+              id="userEmail"
+            />
+          </div>
+          <div className="mb-3">
+            <label for="userPhone" className="form-label">
+              <h1>Contact Number</h1>
+            </label>
+            <input
+              type="tel"
+              onChange={(e) => setPhone(e.target.value)}
+              className="form-control"
+              id="userPhone"
+            />
+          </div>
+          <div className="mb-3 form-check">
+            <input
+              type="checkbox"
+              className="form-check-input"
+              id="exampleCheck1"
+            />
+            <label className="form-check-label" for="exampleCheck1">
+              Check me out
+            </label>
+          </div>
+          <button type="submit" className="btn btn-primary">
+            Register
+          </button>
+        </form>
       </div>
     </div>
   );
--- crud-app/src/Components/Read.js
+++ crud-app/src/Components/Read.js
@@ -2,6 +2,22 @@ import React, { useEffect, useState } from "react";
 import axios from "axios";
 
 function Read() {
+  const [data, setData] = useState([]);
+
+  function getData() {
+    axios
+      .get("https://crudcrud.com/api/5cf2ba86a1934575b3a661e18dc71377/formData")
+      .then((res) => {
+        setData(res.data);
+        console.log(res.data);
+      });
+  }
+
+  useEffect(() => {
+    getData();
+    console.log("effect running");
+  }, []);
+
   return (
     <>
       <h1 className="bg-warning p-2">All Registrations</h1>
@@ -16,20 +32,27 @@ function Read() {
             <th scope="col"></th>
           </tr>
         </thead>
-        <tbody>
-          <tr>
-            <th scope="row"></th>
-            <td></td>
-            <td></td>
-            <td></td>
-            <td>
-              <button className="btn btn-success">Edit</button>
-            </td>
-            <td>
-              <button className="btn btn-danger">Delete</button>
-            </td>
-          </tr>
-        </tbody>
+        {data.map((eachData) => {
+          {
+            console.log(eachData);
+          }
+          return (
+            <tbody>
+              <tr>
+                <th scope="row">{eachData?._id}</th>
+                <td>{eachData?.name}</td>
+                <td>{eachData?.email}</td>
+                <td>{eachData?.phone}</td>
+                <td>
+                  <button className="btn btn-success">Edit</button>
+                </td>
+                <td>
+                  <button className="btn btn-danger">Delete</button>
+                </td>
+              </tr>
+            </tbody>
+          );
+        })}
       </table>
     </>
   );
